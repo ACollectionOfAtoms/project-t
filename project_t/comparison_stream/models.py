0a6cca56b49f25e29f970de033f1337fcbfa7373
Adam <davidatomhernandez@gmail.com>
2017-02-22 14:49:36 -0500

    Added 'TEvent' model, implemented psql

--- project_t/comparison_stream/models.py
+++ project_t/comparison_stream/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class TEvent(models.Model):
+    event_description = models.CharField(max_length=100)
+    references = models.CharField(max_length=10000)
+    date = models.DateTimeField('date published')
--- project_t/project_t/settings.py
+++ project_t/project_t/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'comparison_stream.apps.ComparisonStreamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'project_t.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_t',
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Detroit'
 
 USE_I18N = True
 
